9fd9b9e22725ec9cedd346f869e61a87d28e933c
sorensenclara <sorensenclara@gmail.com>
2026-06-03 09:08:41 -0300

    Fix ASGI initialization order

--- padelboard/asgi.py
+++ padelboard/asgi.py
@@ -1,13 +1,14 @@
-
 import os
+
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "padelboard.settings")
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import scoreboard.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'padelboard.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
