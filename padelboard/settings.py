5b7b21fd550e3d38a74e740fb91f7cdd1185611c
sorensenclara <sorensenclara@gmail.com>
2026-06-04 17:50:42 -0300

    Volvemos a lo anterior

--- padelboard/settings.py
+++ padelboard/settings.py
@@ -1,8 +1,6 @@
 
 from pathlib import Path
 import os
-import dj_database_url
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,10 +57,10 @@ CHANNEL_LAYERS = {
 }
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = []
