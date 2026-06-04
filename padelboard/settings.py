46f5dbde8a74270ce2a579cb1de9a3642cd5db1e
sorensenclara <sorensenclara@gmail.com>
2026-06-04 17:42:59 -0300

    Configurar base de datos postgres en render

--- padelboard/settings.py
+++ padelboard/settings.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,10 +59,10 @@ CHANNEL_LAYERS = {
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
